bac6b05cc1a80fe7c05cf546e19ea547a46cdf1b
Meyer McMains <meyer.mcmains@gmail.com>
2017-07-26 21:36:23 -0400

    updated JSON and populate script :goat:

--- js/populate.js
+++ js/populate.js
@@ -1,3 +1,6 @@
+var nameList;
+var ctr = 0;
+
 $( document ).ready(function() {
 	getHeroList();
 });
@@ -6,15 +9,27 @@ function getHeroList() {
 	$.get("../heroJSON/List/HeroList.txt", function(data) {
 		var lines = data.split("\n");
 		lines.pop();
+		nameList = lines;
+		console.log(nameList);
 		populate(lines);
 	}, 'text');
 }
 
 function populate(lines) {
-	for (var i = 0, len = lines.length; i < len; i++) {
-			$('body').append('<video loop class="portrait">' +
-        						'<source src="video/portraits/' + lines[i] +'.mp4" type="video/mp4">' +
-							'</video>')
-		}
+	for(var i = 0, len = lines.length; i < len; i++) {
+			$.getJSON( "../heroJSON/" + lines[i] + ".json", function( data ) {
+	  			$.each(data, function(key, value){
+	  				if(key === "Primary Attribute"){
+	  					displayPortrait(value)
+	  				}
+	  			});
+	  			ctr ++;
+ 			});
+	}
 }
 
+function displayPortrait(attribute) {
+	$('#' + attribute).append('<video loop class="portrait ball">' +
+        						'<source src="video/portraits/' + nameList[ctr] +'.mp4" type="video/mp4">' +
+							'</video>');
+}
