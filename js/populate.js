007f61c9657a4587c8803ec0d1b6a5f3c5843479
Meyer McMains <meyer.mcmains@gmail.com>
2017-07-27 21:55:42 -0400

    More structure, populate js :goat:

--- js/populate.js
+++ js/populate.js
@@ -1,7 +1,7 @@
 var nameList;
 var ctr = 0;
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	getHeroList();
 });
 
@@ -10,7 +10,6 @@ function getHeroList() {
 		var lines = data.split("\n");
 		lines.pop();
 		nameList = lines;
-		console.log(nameList);
 		populate(lines);
 	}, 'text');
 }
@@ -29,7 +28,21 @@ function populate(lines) {
 }
 
 function displayPortrait(attribute) {
-	$('#' + attribute).append('<video loop class="portrait ball">' +
+	$('#' + attribute).append('<video loop class="video portrait">' +
         						'<source src="video/portraits/' + nameList[ctr] +'.mp4" type="video/mp4">' +
 							'</video>');
 }
+
+$('.list').on('mouseover', 'video', function() {
+	$(this).get(0).play();
+	$(this).css('-webkit-clip-path', 'none');
+	$(this).css('max-width', 'none');
+	$(this).css('z-index', '1');
+});
+
+$('.list').on('mouseleave', 'video', function() {
+	$(this).get(0).pause();
+	$(this).css('-webkit-clip-path', 'polygon(30% 0, 82% 0, 82% 100%, 30% 100%)');
+	$(this).css('max-width', '100px');
+	$(this).css('z-index', '0');
+});
